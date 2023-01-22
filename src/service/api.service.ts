37d9af5d1fd56bdb8b07c8aa2b00329f42237ff4
Coreytaz <coreytaz321@gmail.com>
2023-01-22 20:30:28 +0500

    cors

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, append,delete,entries,foreach,get,has,keys,set,values'
+  }
 })
 
 export function apiSetHeader (name: string, value: string): void {
