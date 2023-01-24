5b1da0fe46ef9bd1c2a09422709faf073cb922bc
Coreytaz <coreytaz321@gmail.com>
2023-01-24 22:52:00 +0500

    cors

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -2,14 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'Access-Control-Allow-Credentials': true,
-    'Access-Control-Allow-Origin': ['http://localhost:3000', 'https://react-web-chat.vercel.app'],
-    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-    'Access-Control-Allow-Headers':
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    'Content-Type': 'application/json'
-  }
+  withCredentials: true
 })
 
 export function apiSetHeader (name: string, value: string): void {
@@ -21,6 +14,7 @@ export function apiSetHeader (name: string, value: string): void {
 api.interceptors.request.use(config => {
   // if (config.defaults.headers.Authorization != null) {
   // }
+  config.withCredentials = true
   return config
 }, async error => {
   return await Promise.reject(error)
